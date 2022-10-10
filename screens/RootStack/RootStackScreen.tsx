9c6212b555cbe39606da7598bf60ac844a084e5e
aniruthn <aniruth@retool.com>
2022-10-09 20:16:51 -0700

    update to modal options

--- screens/RootStack/RootStackScreen.tsx
+++ screens/RootStack/RootStackScreen.tsx
@@ -33,7 +33,10 @@ export function RootStackScreen() {
   const options = { headerShown: false };
   return (
     <NavigationContainer>
-      <RootStack.Navigator mode="modal" initialRouteName="Main">
+      <RootStack.Navigator
+        screenOptions={{ presentation: "modal" }}
+        initialRouteName="Main"
+      >
         <RootStack.Screen
           name="Main"
           component={MainStackScreen}
